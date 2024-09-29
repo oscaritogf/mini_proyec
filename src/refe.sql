abd68b7796f2e9c2412ba82052a7a1db25266036
oscaritogf <oigarciaf@unah.hn>
2024-09-28 22:54:00 -0600

    base de dato

--- src/refe.sql
+++ src/refe.sql
@@ -1,3 +1,4 @@
+-- Crear usuario
 CREATE OR REPLACE FUNCTION create_user(
   p_nombre TEXT,
   p_apellido TEXT,
@@ -11,11 +12,38 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-
+-- Leer usuario por ID
 CREATE OR REPLACE FUNCTION read_user(p_id INT)
 RETURNS SETOF users AS $$
 BEGIN
   RETURN QUERY
   SELECT * FROM users WHERE id = p_id;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Actualizar usuario
+CREATE OR REPLACE FUNCTION update_user(
+  p_id INT,
+  p_nombre TEXT,
+  p_apellido TEXT,
+  p_correo TEXT
+) RETURNS SETOF users AS $$
+BEGIN
+  RETURN QUERY
+  UPDATE users
+  SET nombre = p_nombre, apellido = p_apellido, correo = p_correo
+  WHERE id = p_id
+  RETURNING *;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Eliminar usuario
+CREATE OR REPLACE FUNCTION delete_user(p_id INT)
+RETURNS SETOF users AS $$
+BEGIN
+  RETURN QUERY
+  DELETE FROM users
+  WHERE id = p_id
+  RETURNING *;
 END;
 $$ LANGUAGE plpgsql;
